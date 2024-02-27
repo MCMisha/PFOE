cd5e33d7a0e844b3576b77d473ac1bf04944afe7
misha <misha.yakushevich4@gmail.com>
2024-02-28 00:05:49 +0100

    PFOE-4 Dodanie klasy do obsługi bazy danych na backendzie oraz modelów

---
+++ backend/WebApi/WebApi/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Models
+{
+    public partial class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Location { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public DateOnly Date { get; set; }
+        public int ParticipantNumber { get; set; }
+        public int Organizer { get; set; }
+        public int VisitsNumber { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
