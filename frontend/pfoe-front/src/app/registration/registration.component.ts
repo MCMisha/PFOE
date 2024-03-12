82ede2e97a42a71b51c41de2ec6bb45f79577b79
misha <misha.yakushevich4@gmail.com>
2024-03-12 13:10:47 +0100

    PFOE-27 Dodanie formularza do rejestracji

--- frontend/pfoe-front/src/app/registration/registration.component.ts
+++ frontend/pfoe-front/src/app/registration/registration.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
+  registrationForm = this.fb.group({
+    firstName: ['', [Validators.required, Validators.maxLength(30)]],
+    lastName: ['', [Validators.required, Validators.maxLength(40)]],
+    email: ['', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'), Validators.maxLength(50)]],
+    login: ['', [Validators.required, Validators.maxLength(50)]],
+    password: ['', [Validators.required, Validators.maxLength(50)]],
+    repeatPassword: ['', [Validators.required, Validators.maxLength(50)]]
+  });
+  constructor(private fb: FormBuilder) {
+  }
 
+  onRegisterClick() {
+    console.log(this.registrationForm.value);
+  }
 }
--- frontend/pfoe-front/src/app/registration/registration.module.ts
+++ frontend/pfoe-front/src/app/registration/registration.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationComponent } from './registration.component';
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
 
 
 @NgModule({
@@ -11,7 +16,16 @@ import { RegistrationComponent } from './registration.component';
   ],
   imports: [
     CommonModule,
-    RegistrationRoutingModule
+    RegistrationRoutingModule,
+    MatLabel,
+    MatFormField,
+    MatError,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    ReactiveFormsModule,
+    MatInput,
+    MatButton
   ]
 })
 export class RegistrationModule { }
