ea36a0489f6f55800d1c54b7ff244a4ec8137f11
HubertMos99 <hubertmos99@gmail.com>
2024-03-19 12:19:54 +0100

    Komponent logowania

--- frontend/pfoe-front/src/app/login/login.component.ts
+++ frontend/pfoe-front/src/app/login/login.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, Validators} from '@angular/forms';
+import {UserService} from "../services/user.service";
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {catchError, debounceTime, of, Subscription, switchMap} from "rxjs";
+import {User} from "../models/user";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy{
+  loginForm = this.fb.group({
+    login: ['', [Validators.required, Validators.maxLength(50)]],
+    password: ['', [Validators.required, Validators.maxLength(50)]],
+  }, {
 
+  });
+
+
+  subscription: any = new Subscription();
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private userService: UserService) {
+  }
+
+  ngOnInit() {
+
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  onLoginClick() {
+    const _login: string = this.loginForm.value.login!;
+    const _password: string = this.loginForm.value.password!;
+
+    this.userService.login(_login, _password).subscribe(res => {
+      console.log(res);
+      this.openSnackBar('Udało się zalogować', 'OK');
+    }, error => {
+      this.openSnackBar(error.error.message, 'OK');
+    });
+  }
+
+  private openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action);
+  }
 }
--- frontend/pfoe-front/src/app/login/login.module.ts
+++ frontend/pfoe-front/src/app/login/login.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 
 @NgModule({
@@ -11,7 +16,17 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule,
+    MatButton,
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatError,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
