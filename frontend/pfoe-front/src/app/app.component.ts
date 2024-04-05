8a0045c0ff9a2ee4e80992fc438482425e411dd3
misha <misha.yakushevich4@gmail.com>
2024-04-05 23:51:06 +0200

    PFOE-24 Obsługa zalogowanego użytkownika po stronie frontend'u

--- frontend/pfoe-front/src/app/app.component.ts
+++ frontend/pfoe-front/src/app/app.component.ts
@@ -1,22 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {UserService} from "./services/user.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'pfoe-front';
   menuItems = [
     {path: '', name: 'Strona główna'},
     {path: 'events', name: 'Wydarzenia'}
   ];
   search = new FormControl('');
+  login: string = '';
+  subscription = new Subscription();
+  isLoggedIn: any = false;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.login = localStorage.getItem('login') || '';
+    this.subscription.add(
+      this.userService.loginSuccess.subscribe(login => {
+        if(this.login === '') {
+          this.login = login;
+        }
+        this.isLoggedIn = true;
+        localStorage.setItem('login', login);
+      })
+    );
+    this.subscription.add(
+      this.userService.isLoggedIn(this.login).subscribe(res =>
+      {
+        this.isLoggedIn = Boolean(res);
+        console.log('this.isLoggedIn', this.isLoggedIn);
+      })
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   onKeyDown($event: KeyboardEvent) {
     if ($event.key === 'Enter') {
       console.log(this.search.value);
     }
+  }
+
+  logOut() {
+    console.log(this.login);
+
   }
 }
