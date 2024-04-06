f437bcf9a7b2240e08de2ea955fe2c0f6467dd5e
olafmyszak <myszakolaf@gmail.com>
2024-04-07 01:31:22 +0200

    [PFOE-53] Poprawa misclicka w EventRepository

--- backend/WebApi/WebApi/Repositories/EventRepository.cs
+++ backend/WebApi/WebApi/Repositories/EventRepository.cs
@@ -49,12 +49,11 @@ public class EventRepository
 
     public List<Event> Search(string query)
     {
-        return _appDbContext.Events.FromSqlInterpolated(
-                $"SELECT id, name, location, organizer, visits_number, creation_date, category, date, participant_number FROM pfoe.event WHERE event.document_idx_col @@ to_tsquery('english', {query})")
-            .ToList();
-        return _appDbContext.Events.FromSqlInterpolated(
-                $"SELECT id, name, location, organizer, visits_number, creation_date, category, date, participant_number FROM pfoe.event WHERE event.document_idx_col @@ to_tsquery('english', {query})")
-            .ToList();
+        // return _appDbContext.Events.FromSqlInterpolated(
+        //         $"SELECT id, name, location, organizer, visits_number, creation_date, category, date, participant_number FROM pfoe.event WHERE event.document_idx_col @@ to_tsquery('english', {query})")
+        //     .ToList();
+
+        return _appDbContext.Events.Where(e => EF.Functions.ToTsVector("english", e.Name + " " + e.Location + " " + e.Category).Matches(EF.Functions.ToTsQuery("english", query))).ToList();
     }
 
     public ActionResult<List<Event>> GetMostPopular()
