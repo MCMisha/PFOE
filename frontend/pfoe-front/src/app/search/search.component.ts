03db79d3bd8b930d0b6bacc360cfdae6f141c0c1
olafmyszak <myszakolaf@gmail.com>
2024-04-09 22:23:48 +0200

    [PFOE-53] Poprawki

--- frontend/pfoe-front/src/app/search/search.component.ts
+++ frontend/pfoe-front/src/app/search/search.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
 import {EventModel} from "../models/eventModel";
 import {SearchService} from "../services/search.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Route, Router} from "@angular/router";
 import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
@@ -11,37 +11,39 @@ import {MatPaginator} from "@angular/material/paginator";
   styleUrl: './search.component.scss'
 })
 
-export class SearchComponent implements OnInit{
+export class SearchComponent implements OnInit {
   dataSource: MatTableDataSource<EventModel> = new MatTableDataSource<EventModel>();
   events: EventModel[] = [];
   columnsToDisplay = ['name', 'location', 'category', 'date', 'participantNumber'];
   @ViewChild(MatPaginator) paginator!: MatPaginator
 
-  constructor(private route:ActivatedRoute, private searchService : SearchService) {
+  constructor(private route: ActivatedRoute, private searchService: SearchService, private router: Router) {
   }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       const query = params['q'];
 
-      this.events = []
-
-      if(query) {
+      if (query) {
         this.search(query);
-        this.dataSource.paginator = this.paginator;
       }
     })
   }
 
-  search(query: string){
+  search(query: string) {
     this.events = []
     this.searchService.search(query).subscribe(res => {
-        if(res  != null) {
+        if (res != null) {
           this.events = res as EventModel[];
           this.dataSource.data = this.events;
           console.log(this.events);
+          setTimeout(() => this.dataSource.paginator = this.paginator);
         }
       }
     )
+  }
+
+  goToEvent(id: number) {
+
   }
 }
