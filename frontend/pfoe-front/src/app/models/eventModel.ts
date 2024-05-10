105ef19618213379dcc10be9834853becbc43a2a
HubertMos99 <hubertmos99@gmail.com>
2024-05-10 19:27:32 +0200

    PFOE-43 Komponent do tworzenia nowego wydarzenia, zakomentowałem wywołanie funkcji createEvent(), wyswietlam zmienne w console.log() bo nie wiem czy jest poprawny format

--- frontend/pfoe-front/src/app/models/eventModel.ts
+++ frontend/pfoe-front/src/app/models/eventModel.ts
@@ -1,11 +1,11 @@
 export class EventModel {
   id?: number = 0;
-  name: string | null | undefined;
-  location: string | null | undefined;
-  category: string | null | undefined;
-  date: string | null | undefined;
-  participantNumber: number | null | undefined;
-  organizer: number | null | undefined;
-  visits_number: number | null | undefined;
-  creation_date: Date | null | undefined;
+  name?: string | null | undefined;
+  location?: string | null | undefined;
+  category?: string | null | undefined;
+  date?: string | null | undefined;
+  participantNumber?: number | null | undefined;
+  organizer?: number | null | undefined;
+  visits_number?: number | null | undefined;
+  creation_date?: Date | null | undefined;
 }
