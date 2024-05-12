a70e0a6a1ddbe4c96867b6aaec369b6a656d8136
HubertMos99 <hubertmos99@gmail.com>
2024-05-12 19:34:54 +0200

    PFOE-47 Komponent do edycji wydarzenia. Nie działa edycja

--- frontend/pfoe-front/src/app/edit-event/edit-event.component.ts
+++ frontend/pfoe-front/src/app/edit-event/edit-event.component.ts
@@ -1,6 +1,12 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {EventModel} from "../models/eventModel";
+import {FormBuilder, Validators} from "@angular/forms";
+import {EventService} from "../services/event.service";
+import {UserService} from "../services/user.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Subscription} from "rxjs";
+import {eventCategoriesArray} from "../enums/event-category";
 
 @Component({
   selector: 'app-edit-event',
@@ -9,12 +15,77 @@ import {EventModel} from "../models/eventModel";
 })
 export class EditEventComponent implements OnInit, OnDestroy {
     event?: EventModel;
-    constructor(private route: ActivatedRoute) {
+
+  editEventForm = this.fb.group({
+    event_id: [0],
+    eventName: ['', [Validators.required, Validators.maxLength(50)]],
+    eventCategory: ['', [Validators.required]],
+    eventDate: ['', [Validators.required]],
+    eventMaxParticipants: ['', [Validators.required]],
+    eventLocation: ['', [Validators.required, Validators.maxLength(50)]],
+    eventOrganizer: [0],
+    eventVisit_number: [0],
+    creation_date: [new Date()]
+  });
+
+  subscription: any = new Subscription();
+  isLoading: boolean = false;
+  login: string = '';
+  categories = eventCategoriesArray;
+  todaysDate = new Date();
+
+    constructor(
+      private route: ActivatedRoute,
+      private eventService: EventService,
+      private snackBar: MatSnackBar,
+      private fb: FormBuilder,
+      private router: Router
+    ) {
+
     }
 
     ngOnInit(): void {
       this.event = this.route.snapshot.data['event'];
+
+      this.editEventForm.patchValue({
+        event_id: this.event?.id,
+        eventName: this.event?.name,
+        eventCategory: this.event?.category,
+        eventDate: this.event?.date,
+        eventMaxParticipants: this.event?.participantNumber?.toString(), // Convert number to string
+        eventLocation: this.event?.location,
+        eventOrganizer: this.event?.organizer,
+        eventVisit_number: this.event?.visits_number,
+        creation_date: this.event?.creation_date,
+      });
+
     }
     ngOnDestroy(): void {
+      this.subscription.unsubscribe();
     }
+
+  onEditEventClick(){
+
+      /*
+      this.event = {
+        name: this.editEventForm.value.eventName,
+        category: this.editEventForm.value.eventCategory,
+        date: this.editEventForm.value.eventDate,
+        participantNumber: Number(this.editEventForm.value.eventMaxParticipants),
+        location: this.editEventForm.value.eventLocation,
+      }
+      */
+
+    this.isLoading = true;
+    this.eventService.updateEvent(this.editEventForm.value).subscribe(_ => {
+      this.isLoading = false;
+      this.snackBar.open('Udało się edytować wydarzenie', 'OK');
+      this.router.navigate([''])
+    });
+
+  }
+
+  disabledEditEvent() {
+    return this.editEventForm.invalid || this.isLoading;
+  }
 }
--- frontend/pfoe-front/src/app/models/eventModel.ts
+++ frontend/pfoe-front/src/app/models/eventModel.ts
@@ -1,10 +1,10 @@
 export class EventModel {
   id?: number = 0;
   name?: string | null | undefined;
-  location?: string | null | undefined;
   category?: string | null | undefined;
   date?: string | null | undefined;
   participantNumber?: number | null | undefined;
+  location?: string | null | undefined;
   organizer?: number | null | undefined;
   visits_number?: number | null | undefined;
   creation_date?: Date | null | undefined;
